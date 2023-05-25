cc3e99d809e62422cc6dabd3c31d29090d3c3854
Jalagon13 <A1ag0nJus1in@gmail.com>
2023-05-24 20:26:06 -0700

    Before lighting changes

--- Assets/_Project/_Scripts/Cave Generation/CaveBehavior.cs
+++ Assets/_Project/_Scripts/Cave Generation/CaveBehavior.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 namespace IslandBoy
@@ -9,21 +7,22 @@ namespace IslandBoy
         private CaveLevel _caveLevel;
         private bool _isQuitting;
 
-        public void Initialize()
-        {
-            _caveLevel = transform.parent.transform.parent.transform.GetComponent<CaveLevel>();
-        }
-
         private void OnDestroy()
         {
-            if (_isQuitting) return;
-
-            _caveLevel.TryToSpawnStairs(transform.position);
+            if (!_isQuitting)
+            {
+                _caveLevel.TryToSpawnStairs(transform.position);
+            }
         }
 
         private void OnApplicationQuit()
         {
             _isQuitting = true;
+        }
+
+        public void Initialize()
+        {
+            _caveLevel = transform.parent.transform.parent.transform.GetComponent<CaveLevel>();
         }
     }
 }
