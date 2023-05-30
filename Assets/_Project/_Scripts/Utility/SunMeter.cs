b02e20e938397446cdbfd23a4a744cc41113991b
Jalagon13 <A1ag0nJus1in@gmail.com>
2023-05-29 17:29:46 -0700

    Day time system added

---
+++ Assets/_Project/_Scripts/Utility/SunMeter.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+namespace IslandBoy
+{
+    public class SunMeter : MonoBehaviour
+    {
+        [SerializeField] private float _dayDurationInSec;
+        [SerializeField] private TextMeshProUGUI _timeCounterText;
+
+        private Timer _timer;
+
+        private void Awake()
+        {
+            _timer = new(_dayDurationInSec);
+
+            _timer.OnTimerEnd += EndOfDay;
+        }
+
+        private void Update()
+        {
+            _timer.Tick(Time.deltaTime);
+            _timeCounterText.text = $"Day Time Left: {Mathf.RoundToInt(_timer.RemainingSeconds)}";
+        }
+
+        private void EndOfDay()
+        {
+            Debug.Log("End of day callback");
+        }
+    }
+}
