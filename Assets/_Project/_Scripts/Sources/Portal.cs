e29d6891fba85948c4d197f38f36526d63d944e0
Jalagon13 <A1ag0nJus1in@gmail.com>
2023-05-29 18:02:57 -0700

    Basic Portal Core stuff added

---
+++ Assets/_Project/_Scripts/Sources/Portal.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace IslandBoy
+{
+    public class Portal : MonoBehaviour
+    {
+        public void AddCore()
+        {
+            // check if player has core, if so end the game!
+
+            Debug.Log("Core Added!");
+        }
+    }
+}
