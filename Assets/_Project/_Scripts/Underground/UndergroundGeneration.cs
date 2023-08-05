22ca670a7075c2ec89399e7d606cd19f877ec3e1
Jalagon13 <A1ag0nJus1in@gmail.com>
2023-08-04 18:21:20 -0700

    Basic Generation works!

--- Assets/_Project/_Scripts/Underground/UndergroundGeneration.cs
+++ Assets/_Project/_Scripts/Underground/UndergroundGeneration.cs
@@ -9,16 +9,17 @@ namespace IslandBoy
     public class UndergroundGeneration : MonoBehaviour
     {
         [SerializeField] private int _chunkSideLength = 12;
+        [SerializeField] private TileBase _stoneTile;
         [SerializeField] private Tilemap[] _caveChunkPrefabs;
 
         private Tilemap _undergroundTm;
-        private List<Vector2> _backlog = new();
+        private List<Vector2> _usedPositions = new();
         private static Vector2[,] _spawnPositions = new Vector2[4,4];
+        private bool _generationComplete;
+        private int _lastChunkElement;
         private int _currRowIndex;
         private int _currColIndex;
         private int _direction;
-        private int _lastChunkElement;
-        private bool _generationComplete;
 
         private void Awake()
         {
@@ -40,79 +41,105 @@ namespace IslandBoy
         public void GenerateNewLevel()
         {
             _generationComplete = false;
-
             _undergroundTm.ClearAllTiles();
-
             _currRowIndex = random.Range(0, 4);
             _currColIndex = 0;
+            _usedPositions = new();
 
-            Vector2 firstRoomSpawnPos = _spawnPositions[_currRowIndex, _currColIndex];
-            SpawnChunk(random.Range(0, 2), firstRoomSpawnPos);
+            SpawnChunk(random.Range(0, 2), _spawnPositions[_currRowIndex, _currColIndex]);
 
             _direction = random.Range(1, 6);
 
-            StartCoroutine(Generate());
-
-            // fill in the rest of the rooms with filler rooms
-        }
-
-        private IEnumerator Generate()
-        {
-            int counter = 0;
-
             while (_generationComplete == false)
             {
-                yield return new WaitForSeconds(0.75f);
                 TryGenerateNextRoom();
-                counter++;
-                if (counter > 100)
+            }
+
+            // fill in the rest of the rooms with filler rooms
+            List<Vector2> spawnPosCopy = new();
+
+            for (int i = 0; i < _spawnPositions.GetLength(0); i++)
+            {
+                for (int j = 0; j < _spawnPositions.GetLength(1); j++)
                 {
-                    Debug.Log("counter went over 100");
-                    break;
+                    spawnPosCopy.Add(_spawnPositions[i, j]);
+                }
+            }
+
+            List<Vector2> unUsedPositions = new();
+
+            for (int i = 0; i < _spawnPositions.GetLength(0); i++)
+            {
+                for (int j = 0; j < _spawnPositions.GetLength(1); j++)
+                {
+                    if(!_usedPositions.Contains(_spawnPositions[i, j]))
+                    {
+                        unUsedPositions.Add(_spawnPositions[i, j]);
+                    }
+                }
+            }
+
+            foreach (Vector2 pos in unUsedPositions)
+            {
+                SpawnChunk(4, pos);
+            }
+
+            // add a border around the map
+            var bounds = _undergroundTm.cellBounds;
+            var offset = 5;
+
+            bounds.xMax += offset;
+            bounds.xMin -= offset;
+            bounds.yMax += offset;
+            bounds.yMin -= offset;
+
+            foreach (Vector3Int pos in bounds.allPositionsWithin)
+            {
+                if (_undergroundTm.GetTile(pos) == null)
+                {
+                    _undergroundTm.SetTile(pos, _stoneTile);
                 }
             }
         }
 
         private void TryGenerateNextRoom()
         {
-            // if last room was T1, place next room either T1 or T2 then start algorithm again.
             if(_lastChunkElement == 1 || _lastChunkElement == 3)
             {
-                // if on the last column, 
                 if(_currColIndex == 3)
                 {
-                    // stop algorithm
-                    Debug.Log("Done");
                     _generationComplete = true;
+                    _direction = 0;
                 }
                 else
                 {
-                    // place either T1 or T2 then start algorithm again
                     _currColIndex++;
-                    SpawnChunk(random.Range(1, 3) == 1 ? 3 : 2, _spawnPositions[_currRowIndex, _currColIndex]);
-                    _direction = random.Range(1, 6);
+                    SpawnChunk(random.Range(1, 4) < 3 ? 2 : 3, _spawnPositions[_currRowIndex, _currColIndex]);
+                    _direction = random.Range(1, 5);
+
+                    if (_currRowIndex == 3)
+                        _direction = 1;
+                    else if (_currRowIndex == 0)
+                        _direction = 3;
                 }
 
                 return;
             }
             else if (_direction == 1 || _direction == 2) // 1 & 2 -> generate room up
             {
-                // move row index "up"
                 _currRowIndex--;
 
                 if (_currRowIndex == 0 || _currRowIndex < 0)
                 {
                     _currRowIndex = 0;
 
-                    // spawn a T1 room
                     SpawnChunk(1, _spawnPositions[_currRowIndex, _currColIndex]);
-                    return;
+                    _direction = 3;
                 }
                 else
                 {
                     SpawnChunk(0, _spawnPositions[_currRowIndex, _currColIndex]);
                     _direction = random.Range(1, 4) < 3 ? 1 : 5;
-                    return;
                 }
             }
             else if (_direction == 3 || _direction == 4) // 3 & 4 -> generate room down
@@ -123,15 +150,13 @@ namespace IslandBoy
                 {
                     _currRowIndex = 3;
 
-                    // spawn a T1 room
                     SpawnChunk(1, _spawnPositions[_currRowIndex, _currColIndex]);
-                    return;
+                    _direction = 1;
                 }
                 else
                 {
                     SpawnChunk(0, _spawnPositions[_currRowIndex, _currColIndex]);
                     _direction = random.Range(1, 4) < 3 ? 3 : 5;
-                    return;
                 }
             }
             else if (_direction == 5) // 5 -> generate room right.
@@ -144,9 +169,6 @@ namespace IslandBoy
                 {
                     SpawnChunk(1, _spawnPositions[_currRowIndex, _currColIndex]);
                 }
-                
-                return;
-
             }
         }
 
@@ -154,11 +176,13 @@ namespace IslandBoy
         {
             BoundsInt area = _caveChunkPrefabs[roomType].cellBounds;
             TileBase[] tiles = _caveChunkPrefabs[roomType].GetTilesBlock(area);
-
             area = new BoundsInt(Vector3Int.FloorToInt(spawnPos), area.size);
 
             _undergroundTm.SetTilesBlock(area, tiles);
             _lastChunkElement = roomType;
+
+            if(!_usedPositions.Contains(spawnPos))
+                _usedPositions.Add(spawnPos);
         }
     }
 }
