d0f842bf191e3d07da93c1b2f342def729acecb3
Jalagon13 <A1ag0nJus1in@gmail.com>
2023-09-10 10:56:19 -0700

    Fish rework started

---
+++ Assets/_Project/_Scripts/Entities/Player/Z_Misc/MobSpawner.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace IslandBoy
+{
+    public class MobSpawner : MonoBehaviour
+    {
+        // spawn fishes around player
+    }
+}
