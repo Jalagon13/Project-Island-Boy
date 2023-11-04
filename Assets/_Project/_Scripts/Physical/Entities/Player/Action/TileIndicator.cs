2a496ac802b8feb0bae2d6f4a7d2c0ebaed421b5
Justin Alagon <A1ag0nJus1in@gmail.com>
2023-11-04 15:39:05 -0700

    Before mouse slot rework

--- Assets/_Project/_Scripts/Physical/Entities/Player/Action/TileIndicator.cs
+++ Assets/_Project/_Scripts/Physical/Entities/Player/Action/TileIndicator.cs
@@ -51,6 +51,9 @@ namespace IslandBoy
 
         public void UpdateLogic()
         {
+            // if mouse item has an item, override _selectedSlot with Mouse slot.
+
+            // if mouse has no item, update selected slot again.
             if (_selectedSlot == null) return;
 
             ChangeToOff();
