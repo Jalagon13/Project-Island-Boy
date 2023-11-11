36c20ab359abc3070acc8395b5f150d9a99f9a8b
Kev <kcjung@uci.edu>
2023-11-11 14:49:29 -0800

    added tree boss (just rusher that cant move rn)

---
+++ Assets/_Project/_Scripts/Physical/Entities/Mob/Treevil/TreevilEntity.cs
@@ -0,0 +1,80 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Pathfinding;
+using UnityEngine.Tilemaps;
+
+namespace IslandBoy
+{
+    public class TreevilEntity : Entity
+    {
+        public AudioClip AgroClip;
+
+        [SerializeField] private float _agroDistance;
+
+        public readonly int HashIdle = Animator.StringToHash("[Anm] TreevilIdle");
+        public readonly int HashMove = Animator.StringToHash("[Anm] TreevilMove");
+        public readonly int HashChase = Animator.StringToHash("[Anm] TreevilChase");
+        public Action OnMove;
+        public Seeker Seeker;
+        public IAstarAI AI;
+
+        protected override void Awake()
+        {
+            base.Awake();
+            AI = GetComponent<IAstarAI>();
+            Seeker = GetComponent<Seeker>();
+        }
+
+        private void OnDisable()
+        {
+            Destroy(gameObject);
+        }
+
+        protected override void Update()
+        {
+            base.Update();
+            OnMove?.Invoke();
+        }
+
+        public void Seek(Vector2 pos)
+        {
+            AI.destination = pos;
+            AI.SearchPath();
+        }
+
+        public void ChangeToIdleState(Animator animator)
+        {
+            AnimStateManager.ChangeAnimationState(animator, HashIdle);
+        }
+
+        public void ChangeToMoveState(Animator animator)
+        {
+            AnimStateManager.ChangeAnimationState(animator, HashMove);
+        }
+
+        public void ChangeToChaseState(Animator animator)
+        {
+            AnimStateManager.ChangeAnimationState(animator, HashChase);
+        }
+
+        public bool PlayerClose()
+        {
+            return Vector3.Distance(gameObject.transform.position, PR.Position) < _agroDistance;
+        }
+
+        public bool CanGetToPlayer()
+        {
+            Path path = ABPath.Construct(gameObject.transform.position, PR.Position);
+
+            // WIP
+            return true;
+        }
+
+        public bool IsStuck()
+        {
+            return AI.velocity.magnitude < 0.2f;
+        }
+    }
+}
