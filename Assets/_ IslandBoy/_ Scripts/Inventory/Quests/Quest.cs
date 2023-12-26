2001f7d96d4b74a34ba468e65b3c7ef5b8b720bc
Jalagon <A1ag0nJus1in@gmail.com>
2023-12-26 14:25:16 -0800

    Quest System done

--- Assets/_ IslandBoy/_ Scripts/Inventory/Quests/Quest.cs
+++ Assets/_ IslandBoy/_ Scripts/Inventory/Quests/Quest.cs
@@ -1,16 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
+using MoreMountains.Tools;
+using Sirenix.OdinInspector;
 using UnityEngine;
 
 namespace IslandBoy
 {
-	public class Quest : MonoBehaviour
+	public class Quest : SerializedMonoBehaviour
 	{
-		[SerializeField] private List<ChestInvSlot> _questItems;
+		[SerializeField] private AudioClip _slotCompleteSound;
+		[SerializeField] private AudioClip _questCompleteSound;
+		[SerializeField] private Dictionary<ItemObject, int> _questItems = new();
 		
-		// public bool SatisfiesReq(MouseSlot mouseSlot)
-		// {
-		// 	if(_questItems.Contains())
-		// }
+		public Dictionary<ItemObject, int> QuestItems => _questItems;
+		
+		public void RemoveReq(ItemObject item)
+		{
+			_questItems.Remove(item);
+			
+			if(_questItems.Count <= 0)
+			{
+				CompleteQuest();
+				return;
+			}
+			
+			MMSoundManagerSoundPlayEvent.Trigger(_slotCompleteSound, MMSoundManager.MMSoundManagerTracks.UI, default);
+		}
+		
+		private void CompleteQuest()
+		{
+			Debug.Log("Quest Complete :D");
+			MMSoundManagerSoundPlayEvent.Trigger(_questCompleteSound, MMSoundManager.MMSoundManagerTracks.UI, default);
+		}
 	}
 }
