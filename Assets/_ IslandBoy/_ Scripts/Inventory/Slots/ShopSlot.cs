7587a8b4fcb41ea11a652db099521f14609340c0
Jalagon <A1ag0nJus1in@gmail.com>
2023-12-26 14:29:21 -0800

    Shop started

---
+++ Assets/_ IslandBoy/_ Scripts/Inventory/Slots/ShopSlot.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace IslandBoy
+{
+    public class ShopSlot : Slot
+    {
+        public override void OnPointerClick(PointerEventData eventData)
+        {
+            
+        }
+    }
+}
