7d03bc15fcd006dcb7383fa981ffabd428359d05
Jalagon <A1ag0nJus1in@gmail.com>
2024-01-15 12:13:16 -0800

    Cave edits

--- Assets/_ IslandBoy/_ Scripts/Data/Items/Tile Objects/FloorObject.cs
+++ Assets/_ IslandBoy/_ Scripts/Data/Items/Tile Objects/FloorObject.cs
@@ -3,47 +3,49 @@ using UnityEngine;
 
 namespace IslandBoy
 {
-    [CreateAssetMenu(fileName = "New Floor", menuName = "Create Item/New Floor")]
-    public class FloorObject : ItemObject
-    {
-        [SerializeField] private RuleTileExtended _floorTile;
+	[CreateAssetMenu(fileName = "New Floor", menuName = "Create Item/New Floor")]
+	public class FloorObject : ItemObject
+	{
+		[SerializeField] private RuleTileExtended _floorTile;
 
-        public override ToolType ToolType => _baseToolType;
-        public override ArmorType ArmorType => _baseArmorType;
+		public override ToolType ToolType => _baseToolType;
+		public override ArmorType ArmorType => _baseArmorType;
 
-        public override void ExecutePrimaryAction(FocusSlotControl control)
-        {
-            var pos = Vector3Int.FloorToInt(control.CursorControl.gameObject.transform.position);
+		public override void ExecutePrimaryAction(FocusSlotControl control)
+		{
+			var pos = Vector3Int.FloorToInt(control.CursorControl.gameObject.transform.position);
+			
+			if(control.FloorTm.Tilemap == null) return;
 
-            if (!control.WallTm.Tilemap.HasTile(pos) && !control.FloorTm.Tilemap.HasTile(pos))
-            {
-                control.FocusSlot.InventoryItem.Count--;
-                GameSignals.ITEM_DEPLOYED.Dispatch();
-                control.FloorTm.Tilemap.SetTile(pos, _floorTile);
-                MMSoundManagerSoundPlayEvent.Trigger(_floorTile.PlaceSound, MMSoundManager.MMSoundManagerTracks.Sfx, default);
-            }
-        }
+			if (!control.WallTm.Tilemap.HasTile(pos) && !control.FloorTm.Tilemap.HasTile(pos))
+			{
+				control.FocusSlot.InventoryItem.Count--;
+				GameSignals.ITEM_DEPLOYED.Dispatch();
+				control.FloorTm.Tilemap.SetTile(pos, _floorTile);
+				MMSoundManagerSoundPlayEvent.Trigger(_floorTile.PlaceSound, MMSoundManager.MMSoundManagerTracks.Sfx, default);
+			}
+		}
 
-        public override void ExecuteSecondaryAction(FocusSlotControl control)
-        {
+		public override void ExecuteSecondaryAction(FocusSlotControl control)
+		{
 
-        }
+		}
 
-        public override string GetDescription()
-        {
-            float clickDistance = 0;
+		public override string GetDescription()
+		{
+			float clickDistance = 0;
 
-            foreach (var item in DefaultParameterList)
-            {
-                switch (item.Parameter.ParameterName)
-                {
-                    case "ClickDistance":
-                        clickDistance = item.Value;
-                        break;
-                }
-            }
+			foreach (var item in DefaultParameterList)
+			{
+				switch (item.Parameter.ParameterName)
+				{
+					case "ClickDistance":
+						clickDistance = item.Value;
+						break;
+				}
+			}
 
-            return $"* Left Click to place<br>* {clickDistance} build distance<br>{Description}";
-        }
-    }
+			return $"* Left Click to place<br>* {clickDistance} build distance<br>{Description}";
+		}
+	}
 }
--- Assets/_ IslandBoy/_ Scripts/Level/LevelGenerator.cs
+++ Assets/_ IslandBoy/_ Scripts/Level/LevelGenerator.cs
@@ -10,6 +10,7 @@ namespace IslandBoy
 {
 	public class LevelGenerator : MonoBehaviour
 	{
+		[Header("Level Generation")]
 		[SerializeField] private Tilemap _wallTilemap;
 		[SerializeField] private Tilemap _floorTilemap;
 		[SerializeField] private TileBase _floorTile;
@@ -21,29 +22,55 @@ namespace IslandBoy
 		[SerializeField] private float _noiseDensity;
 		[SerializeField] private int _width = 100;
 		[SerializeField] private int _height = 100;
+		[Header("Resource Generation")]
+		[SerializeField] private Clickable _iron;
+		[SerializeField] private TileBase _rscTile;
 		
 		private int[,] _map;
 		
-		private void Start()
+		private Transform _resourceHolder;
+		private List<Vector2Int> _wallSpots;
+		
+		private void Awake()
 		{
-			Generate();
+			_resourceHolder = transform.GetChild(2);
+			_wallSpots = new();
 		}
 		
+		private void Start()
+		{
+			GenerateWallsFloors();
+			GenerateResourceNodes();
+		}
+		
+		#region Map Generation
+		
 		[Button("Generate")]
-		private void Generate()
+		private void GenerateWallsFloors()
 		{
 			Reset();
 			GenerateBlob();
 			GenerateMap();
 			SmoothMap();	
 			CreateTiles();
+			GenerateResourceNodes();
 		}
 
 		[Button("ResetTiles")]
 		private void Reset()
 		{
+			_resourceHolder = transform.GetChild(2);
 			_floorTilemap.ClearAllTiles();
 			_wallTilemap.ClearAllTiles();
+			_wallSpots.Clear();
+			
+			if(_resourceHolder.transform.childCount > 0)
+			{
+				foreach (Transform transform in _resourceHolder.transform)
+				{
+					Destroy(transform.gameObject);
+				}
+			}
 		}
 		
 		void GenerateBlob()
@@ -159,10 +186,11 @@ namespace IslandBoy
 					{
 						// Instantiate and position a wall tile
 						PlaceTile(_wallTilemap, _wallTile, x, y);
+						_wallSpots.Add(new(x, y));
 					}
 					else
 					{
-						// Instantiate and position a floor tile
+						// Instantiate and position an air tile
 						PlaceTile(_wallTilemap, null, x, y);
 					}
 					
@@ -183,6 +211,35 @@ namespace IslandBoy
 			if(!_floorTilemap.HasTile(pos))
 			{
 				PlaceTile(_floorTilemap, _floorTile, x, y);
+			}
+		}
+		
+		#endregion
+		
+		private void GenerateResourceNodes()
+		{
+			int clumpCount = 10;
+			int nodesPerClumpAvg = 20;
+			
+			for (int clump = 0; clump < clumpCount; clump++)
+			{
+				Vector2 clumpPosition = _wallSpots[Random.Range(0, _wallSpots.Count - 1)];
+				int nodeCount = Random.Range(nodesPerClumpAvg - 5, nodesPerClumpAvg + 5);
+
+				for (int node = 0; node < nodesPerClumpAvg; node++)
+				{
+					Vector2 offset = new Vector2(Random.Range(-1, 2), Random.Range(-1, 2));
+					Vector2 nodePosition = clumpPosition + offset;
+
+					// Ensure the node is within the grid boundaries
+					nodePosition.x = Mathf.Clamp(nodePosition.x, 0, _width - 1);
+					nodePosition.y = Mathf.Clamp(nodePosition.y, 0, _height - 1);
+
+					// Instantiate the resource node prefab at the calculated position
+					// var rsc = Instantiate(_iron, nodePosition, Quaternion.identity);
+					// rsc.transform.SetParent(_resourceHolder);
+					PlaceTile(_wallTilemap, _rscTile, (int)nodePosition.x, (int)nodePosition.y);
+				}
 			}
 		}
 	}
