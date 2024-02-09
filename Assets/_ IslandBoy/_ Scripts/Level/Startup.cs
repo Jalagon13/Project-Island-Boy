e50980a749e0d4332ed0254ac55a02abda94266b
Jalagon <A1ag0nJus1in@gmail.com>
2024-02-08 17:51:13 -0800

    Prototypes added

--- Assets/_ IslandBoy/_ Scripts/Level/Startup.cs
+++ Assets/_ IslandBoy/_ Scripts/Level/Startup.cs
@@ -5,19 +5,58 @@ using UnityEngine.SceneManagement;
 
 namespace IslandBoy
 {
-    public class Startup : MonoBehaviour
-    {
-        private void Awake()
+	public class Startup : MonoBehaviour
+	{
+		private void Awake()
+		{
+			StartCoroutine(LoadScenes());
+			// LoadSceneAdditively("MainMenu");
+			// LoadSceneAdditively("Camera");
+		}
+		
+		private IEnumerator LoadScenes()
         {
-            LoadSceneAdditively("MainMenu");
-            LoadSceneAdditively("Camera");
+            yield return StartCoroutine(LoadSceneAdd("Surface"));
+            yield return StartCoroutine(LoadSceneAdd("DeathPanel"));
+            yield return StartCoroutine(LoadSceneAdd("Inventory"));
+            yield return StartCoroutine(LoadSceneAdd("LaunchControl"));
+            yield return StartCoroutine(LoadSceneAdd("LevelControl"));
+            yield return StartCoroutine(LoadSceneAdd("PauseMenu"));
+            yield return StartCoroutine(LoadSceneAdd("Player"));
+            yield return StartCoroutine(LoadSceneAdd("PromptDisplay"));
+            yield return StartCoroutine(LoadSceneAdd("StatsDisplay"));
+            yield return StartCoroutine(LoadSceneAdd("TimeManager"));
+
+            Scene gameWorld = SceneManager.GetSceneByName("Surface");
+            SceneManager.SetActiveScene(gameWorld);
+            SceneManager.UnloadSceneAsync("MainMenu");
+
         }
 
-        private static void LoadSceneAdditively(string sceneName)
+        private IEnumerator LoadSceneAdd(string sceneName)
         {
-            SceneManager.LoadScene(sceneName, LoadSceneMode.Additive);
+            AsyncOperation sceneAsync = new();
+            sceneAsync = SceneManager.LoadSceneAsync(sceneName, LoadSceneMode.Additive);
+            sceneAsync.allowSceneActivation = false;
+
+            while (sceneAsync.progress < 0.9f)
+            {
+                yield return null;
+            }
+
+            sceneAsync.allowSceneActivation = true;
+
+            while (!sceneAsync.isDone)
+            {
+                yield return null;
+            }
         }
 
+		private static void LoadSceneAdditively(string sceneName)
+		{
+			SceneManager.LoadScene(sceneName, LoadSceneMode.Additive);
+		}
 
-    }
+
+	}
 }
