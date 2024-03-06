d9e65e4b0453f3cfbf9e9ca3e0f401ec39391e37
Kev <kcjung@uci.edu>
2024-03-06 00:00:21 -0800

    player respawns on surface now

--- Assets/_ IslandBoy/_ Scripts/Level/LevelControl.cs
+++ Assets/_ IslandBoy/_ Scripts/Level/LevelControl.cs
@@ -9,41 +9,47 @@ namespace IslandBoy
 	public class LevelControl : MonoBehaviour
 	{
 		public static int CaveLevelToLoad = 0;
-		
+
 		private static GameObject DeployParent;
-		
+		private static string SurfaceScene = "Surface";
+		private static bool PlayerDied;
+
 		public static void SetDP(GameObject dp)
 		{
 			DeployParent = dp;
 		}
-		
+
 		public static GameObject GetDP()
 		{
 			return DeployParent;
 		}
-		
+
 		private void Awake()
 		{
 			GameSignals.CHANGE_SCENE.AddListener(ChangeScene);
+            GameSignals.PLAYER_DIED.AddListener(PlayerDiedToggle);
+            GameSignals.DAY_END.AddListener(RespawnProcedure);
 		}
-		
+
 		private void OnDestroy()
 		{
 			GameSignals.CHANGE_SCENE.RemoveListener(ChangeScene);
-		}
-		
+            GameSignals.PLAYER_DIED.RemoveListener(PlayerDiedToggle);
+            GameSignals.DAY_END.RemoveListener(RespawnProcedure);
+        }
+
 		private void ChangeScene(ISignalParameters parameters)
 		{
-			if(parameters.HasParameter("NextScene"))
+			if (parameters.HasParameter("NextScene"))
 			{
 				// Get active scene and disable all the gameobjects in it
 				Scene activeScene = SceneManager.GetActiveScene();
 				EnableAllObjectsInScene(activeScene, false);
-				
+
 				// Load next scene additively
 				string name = (string)parameters.GetParameter("NextScene");
 				Scene nextScene = SceneManager.GetSceneByName(name);
-				if(nextScene.isLoaded)
+				if (nextScene.isLoaded)
 				{
 					SceneManager.SetActiveScene(nextScene);
 					EnableAllObjectsInScene(nextScene, true);
@@ -54,7 +60,7 @@ namespace IslandBoy
 				}
 			}
 		}
-		
+
 		private void EnableAllObjectsInScene(Scene scene, bool _)
 		{
 			GameObject[] allObjects = scene.GetRootGameObjects();
@@ -63,14 +69,14 @@ namespace IslandBoy
 				go.SetActive(_);
 			}
 		}
-		
+
 		private IEnumerator TransitionScene(string nextScene)
 		{
 			yield return StartCoroutine(LoadSceneAdd(nextScene));
 			Scene scene = SceneManager.GetSceneByName(nextScene);
 			SceneManager.SetActiveScene(scene);
 		}
-		
+
 		private IEnumerator LoadSceneAdd(string sceneName)
 		{
 			AsyncOperation sceneAsync = new();
@@ -89,5 +95,23 @@ namespace IslandBoy
 				yield return null;
 			}
 		}
+
+		private void PlayerDiedToggle(ISignalParameters parameters)
+		{
+			PlayerDied = true;
+		}
+
+
+        private void RespawnProcedure(ISignalParameters parameters)
+        {
+			if (!PlayerDied) return;
+			
+			PlayerDied = false;
+
+            Signal signal = GameSignals.CHANGE_SCENE;
+            signal.ClearParameters();
+			signal.AddParameter("NextScene", SurfaceScene);
+            signal.Dispatch();
+        }
 	}
 }
