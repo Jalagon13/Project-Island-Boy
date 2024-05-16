95a3ffd37625695675d8601eba19bb90fe64df0c
Jalagon <A1ag0nJus1in@gmail.com>
2024-05-16 13:19:38 -0700

    Generation done

---
+++ Assets/_ IslandBoy/_ Scripts/Level/ResourceGenerator.cs
@@ -0,0 +1,132 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+namespace IslandBoy
+{
+	public class ResourceGenerator : MonoBehaviour
+	{
+		[SerializeField] private TilemapObject _grassTm;
+		[SerializeField] private TilemapObject _floorTm;
+		[SerializeField] private TilemapObject _wallTm;
+		[SerializeField] private List<RscSpawnSetting> _rscSpawnSettings;
+		
+		private List<Vector2> _spawnPositions = new();
+		
+		[Serializable]
+		public class RscSpawnSetting
+		{
+			public Clickable ClickableToSpawn;
+			public int SpawnAmount;
+		}
+		
+		private void Awake()
+		{
+			GameSignals.DAY_START.AddListener(RefreshResources);
+		}
+		
+		private void OnDestroy()
+		{
+			GameSignals.DAY_START.RemoveListener(RefreshResources);
+		}
+		
+		private void Start()
+		{
+			// Gather all spawn tile positions
+			BoundsInt bounds = _grassTm.Tilemap.cellBounds;
+			
+			foreach (var position in bounds.allPositionsWithin)
+			{
+				if(_grassTm.Tilemap.HasTile(position))
+				{
+					_spawnPositions.Add(new Vector2(position.x, position.y));
+				}
+			}
+			
+			StartCoroutine(Refresh());
+		}
+		
+		private void RefreshResources(ISignalParameters parameters)
+		{
+			StartCoroutine(Refresh());
+		}
+		
+		private IEnumerator Refresh()
+		{
+			yield return new WaitForSeconds(1f);
+			
+			// Destroy all resources
+			foreach (Transform child in transform)
+			{
+				Destroy(child.gameObject);
+			}
+			
+			// spawn resources
+			foreach(RscSpawnSetting entry in _rscSpawnSettings)
+			{
+				for (int i = 0; i < entry.SpawnAmount; i++)
+				{
+					tryAgain:
+					var randPos = GetRandomPosition();
+					Vector3Int pos = new Vector3Int((int)randPos.x, (int)randPos.y, 0);
+					
+					if(IsValidSpawnPosition(pos))
+					{
+						SpawnClickable(entry.ClickableToSpawn, pos);
+					}
+					else
+					{
+						goto tryAgain;
+					}
+					
+					yield return new WaitForSeconds(0.05f);
+				}
+			}
+		}
+		
+		private Clickable SpawnClickable(Clickable clickable, Vector3Int spawnPos, bool appendToLevel = true)
+		{
+			var r = Instantiate(clickable, spawnPos, Quaternion.identity);
+			
+			if(appendToLevel)
+				r.transform.SetParent(transform);
+				
+			return r;
+		}
+		
+		private Vector2 GetRandomPosition()
+		{
+			int randomIndex = UnityEngine.Random.Range(0, _spawnPositions.Count);
+			Vector2 randomPosition = _spawnPositions[randomIndex];
+			
+			return randomPosition;
+		}
+		
+		private bool IsValidSpawnPosition(Vector3Int pos)
+		{
+			if(_floorTm.Tilemap.HasTile(pos) || _wallTm.Tilemap.HasTile(pos) || !IsClear(pos))
+			{
+				return false;
+			}
+			
+			return true;
+		}
+		
+		public bool IsClear(Vector3Int pos)
+		{
+			var colliders = Physics2D.OverlapBoxAll(new Vector2(pos.x, pos.y) + new Vector2(0.5f, 0.5f), new Vector2(0.5f, 0.5f), 0);
+
+			foreach(Collider2D col in colliders)
+			{
+				if(col.gameObject.layer == 3 || col.CompareTag("RSC") || col.TryGetComponent(out FeetTag ft))
+				{
+					return false;
+				}
+			}
+
+			return true;
+		}
+	}
+}
