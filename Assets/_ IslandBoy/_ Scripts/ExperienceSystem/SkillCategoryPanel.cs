8cce03351cf6f5216f80d9b89e30f2c236757a9a
Kev <kcjung@uci.edu>
2024-05-16 01:11:50 -0700

    added feel to day transition

--- Assets/_ IslandBoy/_ Scripts/ExperienceSystem/SkillCategoryPanel.cs
+++ Assets/_ IslandBoy/_ Scripts/ExperienceSystem/SkillCategoryPanel.cs
@@ -1,3 +1,4 @@
+using MoreMountains.Feedbacks;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
@@ -15,9 +16,12 @@ namespace IslandBoy
         [SerializeField] private TextMeshProUGUI _levelText;
         [SerializeField] private Transform _equationTransform;
         [SerializeField] private Image _fillImage;
+        [SerializeField] private MMF_Player _appearFeedback;
+        [SerializeField] private MMF_Player _expFeedback;
+        [SerializeField] private MMF_Player _levelUpFeedback;
         private LevelSystem _levelSystem;
         private Category _category;
-        private int _convertedExp;
+        private int _convertedExp, _currentLevel;
         
         public void Initialize(Category category, float multiplier)
         {
@@ -25,6 +29,7 @@ namespace IslandBoy
             _category = category;
             _levelSystem = category.LevelSystem;
             _convertedExp = Mathf.RoundToInt(_category.StoredExp * multiplier);
+            _currentLevel = _category.LevelSystem.CurrentLevel;
             
             UpdateTime();
             
@@ -32,8 +37,10 @@ namespace IslandBoy
             _storedExpText.text = $"{_category.StoredExp}xp";
             _multText.text = $"x{multiplier}";
             _convertedExpText.text = $"+{_convertedExp}xp";
-            _levelText.text = $"Lv.{category.LevelSystem.CurrentLevel}";
+            _levelText.text = $"Lv.{_currentLevel}";
             _equationTransform.gameObject.SetActive(_category.StoredExp > 0);
+
+            _appearFeedback?.PlayFeedbacks();
             
             if(_category.StoredExp > 0)
             {
@@ -50,9 +57,18 @@ namespace IslandBoy
             {
                 _levelSystem.GainExperience(1);
                 // Debug.Log(_category.Skill + " " + _levelSystem.CurrentExp);
-                UpdateTime();
+                if (_levelSystem.CurrentLevel > _currentLevel) // level up!
+                {
+                    _currentLevel = _levelSystem.CurrentLevel;
+                    _levelText.text = $"Lv.{_currentLevel}";
+                    _levelUpFeedback?.PlayFeedbacks();
+                }
+                else 
+                    _expFeedback?.PlayFeedbacks();
+
                 _convertedExpText.text = $"+{displayValue - i}xp";
-                _levelText.text = $"Lv.{_category.LevelSystem.CurrentLevel}";
+
+                UpdateTime();
                 
                 yield return new WaitForSeconds(0.2f);
             }
@@ -62,11 +78,10 @@ namespace IslandBoy
         
         public void UpdateTime()
         {
-            int currentLevel = _levelSystem.CurrentLevel;
             int currentExp = _levelSystem.CurrentExp;
             
-            int currentLevelAmount = _levelSystem.ExpPerLevel[currentLevel];
-            int nextLevelAmount = _levelSystem.ExpPerLevel[currentLevel + 1];
+            int currentLevelAmount = _levelSystem.ExpPerLevel[_currentLevel];
+            int nextLevelAmount = _levelSystem.ExpPerLevel[_currentLevel + 1];
             
             _fillImage.fillAmount = Mathf.Clamp01(Mathf.InverseLerp(0, nextLevelAmount - currentLevelAmount, currentExp - currentLevelAmount));
         }
